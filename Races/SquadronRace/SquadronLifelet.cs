bcd3aa51bc7d6e537a0e4b7c38979e52149fe937
Stephan Krusi <stephan.krusi@gmail.com>
2012-04-15 17:10:26 +0200

    New Lifelet class from Stephan

---
+++ Races/SquadronRace/SquadronLifelet.cs
@@ -0,0 +1,93 @@
+
+using System;
+using LifeSimulation.Core;
+
+namespace LifeSimulation.Races.SquadronRace
+{
+
+
+	public class SquadronLifelet : Lifelet
+	{
+		
+		#region Private Variables
+		
+		private Vector _direction; 					// The current direction of our lifelet
+		private Vector _randDirection;				// Just a helpful random Vector
+		private Vector _mapCenter;
+		private double _speed; 						// Our speed we want
+		
+		
+		private long _leaderUID = -1; 				// Squadron leader
+		private ShelledLifelet leader;
+		
+		#endregion
+		
+		
+		
+		#region Properties - these can be accessed by anyone and are visible on the debug display
+		
+		public override string Race {
+			get { return "Test";}
+		}
+		
+		public override string Author {
+			get { return "stephinity";}
+		}
+		
+		#endregion
+		
+		
+		#region Public Methods
+
+
+		public SquadronLifelet (World world, Vector pos) : base(world,pos)
+		{
+			// Init
+			_speed = 2.0;
+			_randDirection = new Vector(this.RandomGen.Next(-1,2),this.RandomGen.Next(-1,2));
+			_mapCenter = new Vector(0 - this.X, 0 - this.Y);
+			
+			_direction = _mapCenter;
+			
+			// Initial message to find each other
+			talk('!');
+			
+		}
+		
+		public Vector formCircle( Double radius, Vector center) {
+			double phi = this.RandomGen.Next(0,2) * 2*Math.PI;
+			return new Vector(radius * Math.Cos(phi) - center.X, radius * Math.Cos(phi) - center.Y);
+		}
+		
+		public override void Simulate() {
+			base.Simulate();
+			
+			// Find the squadron leader
+			foreach(Message message in audibleMessages()) { 
+				if(message.Contents == '!') {
+					if(message.Sender.UID < this.UID && (_leaderUID == -1 || message.Sender.UID > _leaderUID)) {
+						// Follow this lifelet
+						_leaderUID = message.Sender.UID;
+					}
+				} 
+			}
+			
+			// The squadron leader
+			ShelledLifelet leader = this.getLifeletByUID(_leaderUID);
+			
+			// Guard the squadron leader
+			if (this.UID != _leaderUID) {
+				move(formCircle(3,leader.Position));
+			} else {
+				this.move(_mapCenter);
+			}
+			
+			
+			
+			// See other race Attack!
+			
+		}
+		
+		#endregion
+	}
+}
